64503da290a017884cd4248bc2bb240117ce5a2d
jeffhuze <jeff.hughes.mba@gmail.com>
2024-03-26 00:06:05 -0700

    more commits simple

---
+++ Coursera/PythonForEverybody/Module1/Week3/Test1.py
@@ -0,0 +1,24 @@
+x = 1
+print(x)
+x = x + 1
+print(x)
+# exit()
+
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print('Blastoff!')
+
+nam = input('Who are you?')
+print('Welcome', nam)
+
+for i in range(5):
+    print(i)
+    if i > 2:
+        print('bigger than 2')
+    print('Done with i',i)
+print ('All Done.')
+
+
+exit()
